9dcfb2beef396ca3df4726399b8b7db1d2ccbd82
sm-remal <remal.mozzem@gmail.com>
2026-01-02 21:14:02 +0600

    send user information in mongodb

---
+++ src/actions/server/auth.js
@@ -0,0 +1,36 @@
+"use server"
+import bcrypt from 'bcryptjs';
+import { dbConnect } from "@/lib/dbConnect";
+
+export const postUser = async(payload) => {
+    console.log(payload);
+
+    // 1. Check user is Exist or not
+    const isExist = await dbConnect("users").findOne({email: payload.email})
+    if(isExist){
+        return {
+            success: false,
+            message: "user already exist"
+        }
+    }
+
+
+    const hashedPassword = await bcrypt.hash(payload.password, 10);
+    // 2. Create new user 
+    const newUser = {
+        ...payload,
+        createdAt: new Date().toISOString(),
+        role: "user",
+        password: hashedPassword
+    }
+    console.log(newUser)
+    
+    // 3. User Send to Database
+    const result = await dbConnect("users").insertOne(newUser);
+    if(result.acknowledged){
+        return {
+            success: true,
+            message: `User created with ${result.insertedId.toString()}`,
+        }
+    }
+}
--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { postUser } from "@/actions/server/auth";
+
 const RegisterForm = () => {
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     const form = e.target;
@@ -15,7 +17,8 @@ const RegisterForm = () => {
       bloodgroup: form.bloodgroup.value,
     };
 
-    console.log("Submitted Data:", formData);
+    const result = await postUser(formData);
+    alert(result.message)
   };
 
   const inputClass =
